a7578f6b7545b3aa1f3f8666163f8c5b9ca2e6ce
M0rdhau <dachimshvidobadze@gmail.com>
2020-06-16 18:57:54 +0400

    ladies and gentlemen. The Parallax is here.

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -5,23 +5,33 @@ using UnityEngine;
 public class FollowCamera : MonoBehaviour
 {
     [SerializeField] Transform camera;
-    [SerializeField] GameObject firstForeground;
-    [SerializeField] GameObject secondForeground;
-    [SerializeField]float camSpeed = 0.8f;
+    [SerializeField]float camSpeedX = 0.8f;
+    [SerializeField] float camSpeedY = 0.8f;
 
     Vector3 actualCamPos;
+    private float texUnitSize = 0f;
 
 
     private void Start()
     {
         actualCamPos = camera.position;
+        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+        Texture2D tex = sprite.texture;
+        texUnitSize = tex.width / sprite.pixelsPerUnit;
     }
 
 
     // Update is called once per frame
-    void LateUpdate()
+    void FixedUpdate()
     {
-        Vector3 deltaCam = actualCamPos - camera.position;
+        Vector3 deltaCam = camera.position - actualCamPos;
+        transform.position += new Vector3(deltaCam.x * camSpeedX, deltaCam.y*camSpeedY, 0);
+        actualCamPos = camera.position;
 
+        if (Mathf.Abs(camera.position.x - transform.position.x) >= texUnitSize)
+        {
+            float offsetX = (camera.position.x - transform.position.x) % texUnitSize;
+            transform.position = new Vector3(camera.position.x + offsetX, transform.position.y);
+        }
     }
 }
